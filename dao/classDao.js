745e56c9442e1948c3472b727c09b1ea087885ef
violinW-romens <violin@romenscd.cn>
2017-07-08 11:55:44 +0800

    修改架构

--- dao/classDao.js
+++ dao/classDao.js
@@ -45,28 +45,7 @@ module.exports = (dbName, Anne)=> {
       return ClassMethods.getJoinList({}, null, null, 20, 1, 'id')
         .then((data)=> {
           logger.trace(data);
-          const result = dataStructure.getModel('testClass').displayToSource([{
-            "id": 65,
-            "name": "一年级一班",
-            "grade": "grade_one",
-            "student_class_mappingList": [{
-              "id": 1,
-              "join_time": "2017-08-31T16:00:00.000Z",
-              "class_id": 65,
-              "student_id": 1,
-              "name": "王宇",
-              "age": "15",
-              "sex": "男"
-            }, {
-              "id": 2,
-              "join_time": "2017-08-31T16:00:00.000Z",
-              "class_id": 65,
-              "student_id": 2,
-              "name": "郑圆",
-              "age": "14",
-              "sex": "女"
-            }]
-          }]);
+          const result = dataStructure.getModel('testClass').displayToSource(data);
           res.send(result);
         })
         .catch((error)=> {
